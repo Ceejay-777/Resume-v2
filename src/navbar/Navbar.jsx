93d0f2c06c81903b08fdf7370ad371b0a7ca60e9
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-07-03 18:14:07 +0100

    More CSS

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,23 +1,22 @@
-import React, { useState } from 'react'
-import Cee from '../assets/Cee.svg'
-import MainNav from './MainNav'
-import Sidebar from './Sidebar'
-import Hamburger from '../assets/Icons/hamburger'
-
-
+import React, { useState } from "react";
+import Cee from "../assets/Cee.jsx";
+import MainNav from "./MainNav";
+import Sidebar from "./Sidebar";
+import Hamburger from "../assets/Icons/hamburger";
 
 const Navbar = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <div className="navbar">
-      <img src={Cee} alt="Ceejay Logo" className='ceejay-logo'/>
- 
-      <Hamburger className="random"/>
+      {/* <img src={Cee} alt="Ceejay Logo" className="ceejay-logo" /> */}
+      <Cee />
 
-      <Sidebar setSidebarOpen={setSidebarOpen} sidebarOpen={sidebarOpen}/>
+      <Hamburger sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+      <Sidebar setSidebarOpen={setSidebarOpen} sidebarOpen={sidebarOpen} />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/navbar/Sidebar.jsx
+++ src/navbar/Sidebar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import close from '../assets/Icons/x-mark.svg'
 import { navLinks } from '../data'
 import MainNav from './MainNav'
+import Close from '../assets/Icons/Close'
 
 const Sidebar = ({setSidebarOpen, sidebarOpen}) => {
   return (
-    <div  className={sidebarOpen ?"sidebar-container sidebarOpen" : "sidebar-container"}>
-        <img src={close} alt="Close" onClick={() => setSidebarOpen(false)} className='nav-control nav-close'/>
-        
-        <MainNav />
+     <div  className={sidebarOpen ?"sidebar-container sidebarOpen" : "sidebar-container"}>
+
+      <Close sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/> 
+      
+      <MainNav />
     </div>
   )
 }
