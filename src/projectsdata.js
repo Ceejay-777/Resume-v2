513b1003d0f6e1e951cdacb27a7071a3749c7436
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-07-17 22:52:19 +0100

    commit

--- src/projectsdata.js
+++ src/projectsdata.js
@@ -2,10 +2,11 @@ import card1 from "./assets/Project_images/card_details/01.jpg";
 import card2 from "./assets/Project_images/card_details/02.jpg";
 import card3 from "./assets/Project_images/card_details/03.jpg";
 
-import horizon1 from "./assets/Project_images/horizon/01.JPG"
-import horizon2 from "./assets/Project_images/horizon/02.JPG"
-import horizon3 from "./assets/Project_images/horizon/03.JPG"
-import horizon4 from "./assets/Project_images/horizon/04.JPG"
+import horizon1 from "./assets/Project_images/horizon/horizon1.JPG"
+import horizon2 from "./assets/Project_images/horizon/horizon2.JPG"
+import horizon3 from "./assets/Project_images/horizon/horizon3.JPG"
+import horizon4 from "./assets/Project_images/horizon/horizon4.JPG"
+
 
 
 
@@ -20,6 +21,7 @@ export const projects = [
         id: 2,
         name: "second",
         images: [horizon1, horizon2, horizon3, horizon4],
+        // images: [card1],
         info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptates expedita voluptas iure omnis rerum facilis, cupiditate error itaque necessitatibus iusto accusantium at recusandae quisquam amet reprehenderit quasi, placeat sint.",
     },
     {
