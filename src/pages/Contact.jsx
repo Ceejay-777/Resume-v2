753dad654fc0f19c0691d0e97bd41bb918ca087c
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-07-24 22:36:04 +0100

    commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,15 +2,40 @@ import React, { useState } from "react";
 import Skillbar from "../reuseables/Skillbar";
 
 const Contact = () => {
-  const [fname, setFname] = useState("")
-  const [email, setEmail] = useState("")
-  const [message, setMessage] = useState("")
-  return (
-      <div className="main">
-        <div className="contact-form">
+  const [fname, setFname] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-        </div>
-      </div>);
+  const handleSubmit = () => {
+    return "Okay";
+  };
+  return (
+    <div className="main">
+      <div className="contact-form">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={fname}
+            onChange={(event) => setFname(event.target.value)}
+            placeholder="NAME"
+          />
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            placeholder="EMAIL"
+          />
+          <textarea
+            type="email"
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+            placeholder="EMAIL"
+          ></textarea>
+          <button type="submit" className="btn">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
