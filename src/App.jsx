6fa1d3625cd8a9c301a3966816e69d2b7834b8db
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-09-09 08:18:50 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,33 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import './styles/App.css'
-import "./styles/Navigation.css"
-import "./styles/Experience.css"
-import "./styles/MyProjects.css"
-import "./styles/Skills.css"
-import "./styles/contact.css"
+import "./styles/App.css";
+import "./styles/Navigation.css";
+import "./styles/Experience.css";
+import "./styles/MyProjects.css";
+import "./styles/Skills.css";
+import "./styles/contact.css";
 
-import Home from './pages/Home/Home.jsx'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact'
-import MainSharedLayout from './sharedLayouts/mainSharedLayout'
-
-
-Contact
+import Home from "./pages/Home/Home.jsx";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import MainSharedLayout from "./sharedLayouts/mainSharedLayout";
 
 function App() {
-
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<MainSharedLayout />}>
-            <Route index element={<Home />}/>
-            <Route path='about' element={<About />}/>
-            <Route path='projects' element={<Projects />}>
-              
-            </Route>
-            <Route path='contact' element={<Contact />}/>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="projects" element={<Projects />}></Route>
+            <Route path="contact" element={<Contact />} />
           </Route>
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
