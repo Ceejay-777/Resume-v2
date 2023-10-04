917fbfc0faea948742502e2c608a9d345b1e0115
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-10-04 09:40:50 +0100

    Fix MainSharedLayout filepath

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Home from "./pages/Home/Home.jsx";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-import MainSharedLayout from "./sharedLayouts/mainSharedLayout";
+import MainSharedLayout from "./sharedLayouts/MainSharedLayout";
 
 function App() {
   return (
