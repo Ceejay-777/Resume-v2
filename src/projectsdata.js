7a25cf3f0c2ed8d28d15f6cf2e2f4e6953c87d26
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-10-11 06:26:47 +0100

    Checking .PNG issue

--- src/projectsdata.js
+++ src/projectsdata.js
@@ -7,18 +7,18 @@ import horizon2 from "./assets/Project_images/horizon/horizon2.JPG";
 import horizon3 from "./assets/Project_images/horizon/horizon3.JPG";
 import horizon4 from "./assets/Project_images/horizon/horizon4.JPG";
 
-import faqAccordion1 from "./assets/Project_images/faq_accordion/shot1.PNG";
-import faqAccordion2 from "./assets/Project_images/faq_accordion/shot2.PNG";
-import faqAccordion3 from "./assets/Project_images/faq_accordion/shot3.PNG";
-import faqAccordion4 from "./assets/Project_images/faq_accordion/shot4.PNG";
+// import faqAccordion1 from "./assets/Project_images/faq_accordion/shot1.PNG";
+// import faqAccordion2 from "./assets/Project_images/faq_accordion/shot2.PNG";
+// import faqAccordion3 from "./assets/Project_images/faq_accordion/shot3.PNG";
+// import faqAccordion4 from "./assets/Project_images/faq_accordion/shot4.PNG";
 
-import ratingCard1 from "./assets/Project_images/rating_component_card/shot1.PNG";
-import ratingCard2 from "./assets/Project_images/rating_component_card/shot2.PNG";
-import ratingCard3 from "./assets/Project_images/rating_component_card/shot3.PNG";
-import ratingCard4 from "./assets/Project_images/rating_component_card/shot4.PNG";
+// import ratingCard1 from "./assets/Project_images/rating_component_card/shot1.PNG";
+// import ratingCard2 from "./assets/Project_images/rating_component_card/shot2.PNG";
+// import ratingCard3 from "./assets/Project_images/rating_component_card/shot3.PNG";
+// import ratingCard4 from "./assets/Project_images/rating_component_card/shot4.PNG";
 
-import flipCard1 from "./assets/Project_images/flip_card/shot1.PNG";
-import flipCard2 from "./assets/Project_images/flip_card/shot2.PNG";
+// import flipCard1 from "./assets/Project_images/flip_card/shot1.PNG";
+// import flipCard2 from "./assets/Project_images/flip_card/shot2.PNG";
 
 export const projects = [
   {
@@ -35,25 +35,25 @@ export const projects = [
     info: "An interactive card details form page built with HTML, CSS and Javascript. It uses JQuery for DOM traversing and JQueryFormValidation to validate the form. In this project, I learnt a lot about using the JQuery library.",
     skills: ["HTML", "CSS", "JavaScript", "JQuery", "Git"],
   },
-  {
-    id: 3,
-    name: "FAQ accordion component",
-    images: [faqAccordion1, faqAccordion2, faqAccordion3, faqAccordion4],
-    info: "An accordion component built with HTML, CSS, Javacript and JQuery. In this project, I was able to practice a lot with the Javascript language and JQuery and I learnt a lot about the Document Object Model (DOM) and it's manipulation.",
-    skills: ["HTML", "CSS", "JavaScript", "JQuery"],
-  },
-  {
-    id: 4,
-    name: "Rating card component",
-    images: [flipCard1, flipCard2],
-    info: "A rating component card. A simple project from which I learnt a lot about CSS pseudo-elements and pseudo-classes.",
-    skills: ["HTML", "CSS"],
-  },
-  {
-    id: 5,
-    name: "Flip Card",
-    images: [ratingCard1, ratingCard2, ratingCard3, ratingCard4],
-    info: "A fun to watch ATM card that flips on mouse-hover, built with only HTML and CSS.",
-    skills: ["HTML", "CSS", "JavaScript", "JQuery"],
-  },
+  // {
+  //   id: 3,
+  //   name: "FAQ accordion component",
+  //   images: [faqAccordion1, faqAccordion2, faqAccordion3, faqAccordion4],
+  //   info: "An accordion component built with HTML, CSS, Javacript and JQuery. In this project, I was able to practice a lot with the Javascript language and JQuery and I learnt a lot about the Document Object Model (DOM) and it's manipulation.",
+  //   skills: ["HTML", "CSS", "JavaScript", "JQuery"],
+  // },
+  // {
+  //   id: 4,
+  //   name: "Rating card component",
+  //   images: [flipCard1, flipCard2],
+  //   info: "A rating component card. A simple project from which I learnt a lot about CSS pseudo-elements and pseudo-classes.",
+  //   skills: ["HTML", "CSS"],
+  // },
+  // {
+  //   id: 5,
+  //   name: "Flip Card",
+  //   images: [ratingCard1, ratingCard2, ratingCard3, ratingCard4],
+  //   info: "A fun to watch ATM card that flips on mouse-hover, built with only HTML and CSS.",
+  //   skills: ["HTML", "CSS", "JavaScript", "JQuery"],
+  // },
 ];
