a62f03286af914a1e81c7f439ef468077eda3d86
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-01-24 20:17:05 +0100

    commit

--- src/navigation/Navbar.jsx
+++ src/navigation/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Cee from "../assets/Cee.jsx";
 import MainNav from "../reuseables/MainNav.jsx";
 import Sidebar from "./Sidebar";
-import Hamburger from "../assets/Icons/hamburger"
+import Hamburger from "../assets/Icons/hamburger.jsx";
 
 const Navbar = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
