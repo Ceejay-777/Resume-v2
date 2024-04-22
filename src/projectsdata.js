eb04c727c47cee8006a1e50a43a5c08fbf60eb15
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-04-22 14:09:05 +0100

    commit

--- src/projectsdata.js
+++ src/projectsdata.js
@@ -12,28 +12,7 @@ export const projects = [
     image: gridale,
     info: "Gridale is a dynamic, visually appealing game created using React and styled with Tailwind CSS, offering players an engaging, modern user experience as they navigate through a grid-based challenge.",
     skills: ["ReactJS", "Tailwind", "JavaScript", "Git"],
-    link: "https://gridale.vercel.app"
-  },
-  {
-    id: 2,
-    name: "Horizon landing page",
-    image: horizon1,
-    info: "Horizon is a fictional mobile device company. The horizon landing page showcases their latest product (The Horizine Note 3.0) and all it's cool specs. I learnt a lot about CSS flexbox and CSS grid while building this project.",
-    skills: ["HTML", "CSS", "JavaScript", "Git"],
-  },
-  {
-    id: 3,
-    name: "Interactive card details form",
-    image: cardDetails1,
-    info: "An interactive card details form page built with HTML, CSS and Javascript. It uses JQuery for DOM traversing and JQueryFormValidation to validate the form. In this project, I learnt a lot about using the JQuery library.",
-    skills: ["HTML", "CSS", "JavaScript", "JQuery", "Git"],
-  },
-  {
-    id: 4,
-    name: "FAQ accordion component",
-    image: faqAccordion1,
-    info: "An accordion component built with HTML, CSS, Javacript and JQuery. In this project, I was able to practice a lot with the Javascript language and JQuery and I learnt a lot about the Document Object Model (DOM) and it's manipulation.",
-    skills: ["HTML", "CSS", "JavaScript", "JQuery"],
+    link: "https://gridale.vercel.app",
   },
   {
     id: 5,
@@ -41,6 +20,31 @@ export const projects = [
     image: flipCard1,
     info: "A fun to watch ATM card that flips on mouse-hover, built with only HTML and CSS.",
     skills: ["HTML", "CSS", "JavaScript", "JQuery"],
+    link: "https://ceejay-777.github.io/Card-Flip",
+  },
+  {
+    id: 3,
+    name: "Interactive card details form",
+    image: cardDetails1,
+    info: "An interactive card details form page built with HTML, CSS and Javascript. It uses JQuery for DOM traversing and JQueryFormValidation to validate the form. In this project, I learnt a lot about using the JQuery library.",
+    skills: ["HTML", "CSS", "JavaScript", "JQuery", "Git"],
+    link: "https://ceejay-777.github.io/Card-Detail_Form",
+  },
+  {
+    id: 4,
+    name: "FAQ accordion component",
+    image: faqAccordion1,
+    info: "An accordion component built with HTML, CSS, Javacript and JQuery. In this project, I was able to practice a lot with the Javascript language and JQuery and I learnt a lot about the Document Object Model (DOM) and it's manipulation.",
+    skills: ["HTML", "CSS", "JavaScript", "JQuery"],
+    link: "https://ceejay-777.github.io/Faq-accordion-card",
+  },
+  {
+    id: 2,
+    name: "Horizon landing page",
+    image: horizon1,
+    info: "Horizon is a fictional mobile device company. The horizon landing page showcases their latest product (The Horizine Note 3.0) and all it's cool specs. I learnt a lot about CSS flexbox and CSS grid while building this project.",
+    skills: ["HTML", "CSS", "JavaScript", "Git"],
+    link: "https://ceejay-777.github.io/horizon",
   },
   {
     id: 6,
@@ -48,5 +52,6 @@ export const projects = [
     image: ratingCard1,
     info: "A rating component card. A simple project from which I learnt a lot about CSS pseudo-elements and pseudo-classes.",
     skills: ["HTML", "CSS"],
+    link: "https://ceejay-777.github.io/interactive-rating-component-card",
   },
 ];
