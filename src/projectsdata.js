e0281fd1c5d562eb0b84947ae92b1caf22d48c18
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-04-22 14:27:43 +0100

    commit

--- src/projectsdata.js
+++ src/projectsdata.js
@@ -4,6 +4,8 @@ import faqAccordion1 from "./assets/Project_images/faq_accordion/shot1.jpg";
 import ratingCard1 from "./assets/Project_images/rating_component_card/shot1.jpg";
 import flipCard1 from "./assets/Project_images/flip_card/shot1.jpg";
 import gridale from "./assets/Project_images/gridale/gridale.jpg";
+import multistep from "./assets/Project_images/multistep/multistep.jpg";
+import pipeline from "./assets/Project_images/pipeline/pipeline.jpg";
 
 export const projects = [
   {
@@ -15,15 +17,31 @@ export const projects = [
     link: "https://gridale.vercel.app",
   },
   {
-    id: 5,
-    name: "Flip Card",
-    image: flipCard1,
-    info: "A fun to watch ATM card that flips on mouse-hover, built with only HTML and CSS.",
-    skills: ["HTML", "CSS", "JavaScript", "JQuery"],
-    link: "https://ceejay-777.github.io/Card-Flip",
+    id: 2,
+    name: "Pipeline",
+    image: pipeline,
+    info: "Pipeline is a fintech app I worked on with a collaborative team, offering budgeting tools, expense tracking, and real-time insights to simplify financial management for users.",
+    skills: ["ReactJS", "Tailwind", "JavaScript", "Git"],
+    link: "https://https://pipeline-beta.vercel.app",
   },
   {
     id: 3,
+    name: "Multistep subscription card",
+    image: multistep,
+    info: "The multistep game subscription card is a feature that guides users through the process of selecting and subscribing to game services. It streamlines the subscription journey with clear steps and user-friendly navigation, making it easy for users to access and game services.",
+    skills: ["ReactJS", "Tailwind", "JavaScript", "Git"],
+    link: "https://https://multi-step-form-five-phi.vercel.app",
+  },
+  {
+    id: 4,
+    name: "Flip Card",
+    image: flipCard1,
+    info: "A fun to watch ATM card that flips on mouse-hover, built with only HTML and CSS.",
+    skills: ["HTML", "CSS"],
+    link: "https://ceejay-777.github.io/Card-Flip",
+  },
+  {
+    id: 5,
     name: "Interactive card details form",
     image: cardDetails1,
     info: "An interactive card details form page built with HTML, CSS and Javascript. It uses JQuery for DOM traversing and JQueryFormValidation to validate the form. In this project, I learnt a lot about using the JQuery library.",
@@ -31,7 +49,7 @@ export const projects = [
     link: "https://ceejay-777.github.io/Card-Detail_Form",
   },
   {
-    id: 4,
+    id: 6,
     name: "FAQ accordion component",
     image: faqAccordion1,
     info: "An accordion component built with HTML, CSS, Javacript and JQuery. In this project, I was able to practice a lot with the Javascript language and JQuery and I learnt a lot about the Document Object Model (DOM) and it's manipulation.",
@@ -39,7 +57,7 @@ export const projects = [
     link: "https://ceejay-777.github.io/Faq-accordion-card",
   },
   {
-    id: 2,
+    id: 7,
     name: "Horizon landing page",
     image: horizon1,
     info: "Horizon is a fictional mobile device company. The horizon landing page showcases their latest product (The Horizine Note 3.0) and all it's cool specs. I learnt a lot about CSS flexbox and CSS grid while building this project.",
@@ -47,7 +65,7 @@ export const projects = [
     link: "https://ceejay-777.github.io/horizon",
   },
   {
-    id: 6,
+    id: 8,
     name: "Rating card component",
     image: ratingCard1,
     info: "A rating component card. A simple project from which I learnt a lot about CSS pseudo-elements and pseudo-classes.",
