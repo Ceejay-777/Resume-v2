f49915d49254ee9a7142901abb43d80932ec0781
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-06-13 15:19:44 +0100

    Update download CV button

--- src/reuseables/Download.jsx
+++ src/reuseables/Download.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Download = () => {
   return (
-    <a href="./public/Adeosun-Covenant-Joshua.pdf" target='_blank'>
+    <a href="/Adeosun-Covenant-Joshua.pdf" target='_blank'>
       <div className='text-white dark:text-pri font-semibold px-2 py-1 dark:bg-white bg-pri rounded-xl hover:scale-110'>
         Download CV
       </div>
